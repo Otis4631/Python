6817f20fd3b55605adf237141c690faf492aa860
Your Name <you@example.com>
2017-02-15 15:56:41 +0800

    add update_url_list

--- 拉勾网/update.py
+++ 拉勾网/update.py
@@ -1,13 +1,21 @@
 from channel_extract import channel_urls_list
 import update_spider
-
+import time
+from multiprocessing import Pool
 def update_url_list():
+    start_time = time.clock()
+    P = Pool()
     for channel, i in (zip(
             channel_urls_list, range(
                 1, len(channel_urls_list) + 1))):
-        update_spider.get_urls_from(channel, i)
 
+        P.apply_async(func=update_spider.get_urls_from,args=(channel,i))
+    P.close()
+    P.join()
+    end_time = time.clock()
+    ditime = (end_time - start_time) / 60.0
+    print('全部链接已获得，用时{:.4f}min'.format(ditime))
 
+if __name__ == "__main__":
+    update_url_list()
 
-
-update_url_list()
